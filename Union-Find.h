fe56e1edc15f7ebfb4bf02e19a00cac569090261
kltachi <58478335+kltachi@users.noreply.github.com>
2020-01-14 21:36:08 +0200

    Update Union-Find.h

--- Union-Find.h
+++ Union-Find.h
@@ -7,18 +7,6 @@
 
 
 
-#ifdef _DEBUG
-#define DEBUG_CLIENTBLOCK new( _CLIENT_BLOCK, __FILE__, __LINE__)
-#else
-#define DEBUG_CLIENTBLOCK
-#endif // _DEBUG
-
-#ifdef _DEBUG
-#define new DEBUG_CLIENTBLOCK
-#endif
-
-
-
 
 typedef enum {
     SUCCESS_UFC = 0,
