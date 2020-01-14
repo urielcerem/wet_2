ff7f8d5216cb4fab343b00c6121f75a11a18bdde
kltachi <58478335+kltachi@users.noreply.github.com>
2020-01-14 21:36:27 +0200

    Update hash_table.h

--- hash_table.h
+++ hash_table.h
@@ -3,17 +3,6 @@
 #include <math.h>
 
 
-#ifdef _DEBUG
-#define DEBUG_CLIENTBLOCK new( _CLIENT_BLOCK, __FILE__, __LINE__)
-#else
-#define DEBUG_CLIENTBLOCK
-#endif // _DEBUG
-
-#ifdef _DEBUG
-#define new DEBUG_CLIENTBLOCK
-#endif
-
-
 typedef enum {
 	SUCCESS_HT = 0,
 	FAILURE_HT = -1,
